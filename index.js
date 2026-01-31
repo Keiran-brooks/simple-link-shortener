551d9a57ef8e2f8000accebfc76394f4a01ce3dd
keiran-brooks <keiranbrooks@proton.me>
2026-01-31 14:44:13 +0000

    Added base function, //TODO intergrate a DB with redirect URLS

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-let http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('WELCOME! This is short.keiran.club! ...');
-}).listen(8080);
+const http = require('http');
+const fs = require('fs')
+
+
+const server = http.createServer((request, response) => {
+  
+  const { url, method } = request;
+
+  const redirectURL = resolveRequest(url);
+  
+  response.writeHead(308, { 'Content-Type': 'text/plain', 'location': redirectURL});
+  response.end(`You made a ${method} request to ${url}`);
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
+
+function resolveRequest(requestURL){
+  redirectURL = '';
+  
+  console.log(redirectURL);
+  if(requestURL == '/'+'TTT'){
+    redirectURL = 'https://vault.keiran.club';
+  }
+  else{
+    redirectURL = 'https://keiran.club';
+  }
+
+  return redirectURL;
+}
