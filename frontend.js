e1257146280fe3081465c9a1294654233540a151
keiran-brooks <keiranbrooks@proton.me>
2026-02-05 16:30:18 +0000

    new file:   .gitignore 	new file:   example.env 	new file:   frontend.js 	modified:   index.js 	new file:   package-lock.json 	new file:   package.json Added postgres database intergration, project in late final state //TODO: add a front end & api for add short URLs

---
+++ frontend.js
@@ -0,0 +1,1 @@
+//TODO make front end interaction.
--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const pg = require('pg');
 
 
-const server = http.createServer((request, response) => {
-  
+
+const client = new pg.Client({
+  user: process.env.DATABASEUSER,
+  password: process.env.DATABASEPASSWORD,
+  host: process.env.DATABASEHOST,
+  port: process.env.DATABASEPORT,
+  database: process.env.DATABASENAME,
+})
+
+client.connect();
+
+const server = http.createServer(async (request, response) => {
+
   const { url, method } = request;
 
-  const redirectURL = resolveRequest(url);
-  
+  try {
+    const redirectURL = await resolveRequest(url)
+  } 
+  catch (err){
+    console.log(err)
+  }
+
   response.writeHead(308, { 'Content-Type': 'text/plain', 'location': redirectURL});
   response.end(`You made a ${method} request to ${url}`);
 });
@@ -16,15 +33,20 @@ server.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
 });
 
-function resolveRequest(requestURL){
+async function resolveRequest(requestURL){
   redirectURL = '';
-  
-  console.log(redirectURL);
-  if(requestURL == '/'+'TTT'){
-    redirectURL = 'https://vault.keiran.club';
+
+  console.log(requestURL);
+  requestURL = requestURL.replace('/', '')
+  console.log(requestURL);
+
+  try {
+  	const result = await client.query("SELECT * from links where shorturl = '" + requestURL+"';");
+  	redirectURL = 'https://' + result.rows[0]['longurl'];
   }
-  else{
-    redirectURL = 'https://keiran.club';
+  catch (err) {
+    console.log('DB error: ' + err)
+    console.log('URL doesnt exit or db error')
   }
 
   return redirectURL;
